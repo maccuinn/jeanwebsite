b8313419c77b8b8ff7f63507b431235d0bbe3c86
maccuinn <april.l.hall@gmail.com>
2018-04-25 11:46:17 -0700

    inital setup

---
+++ quilts.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php 
+        include '/templates/head.php';
+    ?>
+<body>
+    <?php
+        include 'templates/header.php';
+    ?>
+    
+    <?php
+        include 'templates/footer.php';
+    ?>
+
+    <?php 
+        include 'templates/scripts.php';
+    ?>
+</body>
+</html>
---
+++ templates/head.php
@@ -0,0 +1,13 @@
+<?php 
+if(!isset($pgName)){
+    $pgName = '';
+}
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../styles/styles.css" type="text/css" rel="stylesheet">
+    <title>COMP 1950 - <?php echo $pgName; ?></title>
+</head>
