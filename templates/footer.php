33f03a698d11cd66b184501edfa7da616b5277ce
maccuinn <april.l.hall@gmail.com>
2018-05-02 14:07:35 -0700

    worked on footer styles

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,5 @@
 <div class="footer-container">
-    <div class="social-icons-container">
-        <h4 class="_14">Follow me on</h4>
-        <ul class="social-icons">
-            <li><a href="#0" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
-            <li><a href="#0" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
-            <li><a href="#0" class="social-icon"> <i class="fa fa-google-plus"></i></a></li>
-        </ul>
-    </div>
-    <nav class="secondary-nav">
+    <nav>
         <ul class="col-one">
             <li><a href="#0">Home</a></li>
             <li><a href="#0">Books</a></li> 
@@ -17,5 +9,9 @@
             <li><a href="#0">Contact</a></li>
         </ul>
     </nav>
-    <p>&copy 2018 April Hall</p>
+    <div class="copyright">
+        <p>&copy 2018 April Hall</p>
+        <p>All images &copy Jean Husband</p>
+    </div>
+    
 </div>
