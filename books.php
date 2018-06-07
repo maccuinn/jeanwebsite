b8f60d4fdd729b7bdcf0a176eb1d3d5584bb510d
maccuinn <april.l.hall@gmail.com>
2018-06-07 14:36:24 -0700

    set up book page

--- books.php
+++ books.php
@@ -4,13 +4,75 @@
         include 'templates/head.php';
     ?>
 <body>
-    <?php
-        include 'templates/header.php';
-    ?>
-    
-    <?php
-        include 'templates/footer.php';
-    ?>
+    <header>
+        <?php
+            include 'templates/header.php';
+        ?>
+    </header>
+
+    <div class="book-page">
+    <main>
+        <section class="books">
+            <div class="book-desc">
+                <h2>Millicent Marvel &amp; Bentley Bunny</h2>
+                <h3>A Storybook Quilt</h3>
+                <p class="book-passage">Millicent Marvel left Bentley Bunny outside in the cold last night.<br>Her Gramma will make Bentley a quilt to warm up in but Millicent must choose a pattern.<br>Decisions, decisions.</p>
+                <p class="book-explanation">Discover a new world where animals from the quilt come alive...</p>
+                <p class="book-snippets">Let's stitch a quilt to wrap him up in.<br>Come, pick a pattern from my old cookie tin.</p>
+                <p class="book-snippets">This skunk cabbage pattern might work, don't you think?<br>Uh uh, then Bunny really would stink.</p>
+                <p class="book-explanation">These are just some of the lines to be found in this delightful child’s picture book. The vibrant colors and textures jump from the pages and stimulate the senses of children and quilters alike. The book evolved from a quilt and poem I created for one of my grand-daughters.</p>
+            </div>
+
+            <div class="container">
+                <div class="book-images">
+                    <img class="book-cover-img" src="imgs/surprise-cover-small.jpg" alt="Book Cover">
+                    <div class="thumbnails">
+                        <img src="imgs/skunk-thumbnail.jpg" alt="Skunk page">
+                        <img src="imgs/hippo-thumbnail.jpg" alt="Hippo page">
+                        <img src="imgs/centipede-thumbnail.jpg" alt="Centipede page">
+                    </div>
+                </div>  
+                <div class="buttons">
+                    <p class="more-info-button">More Books</p>
+                    <p class="purchase-button">Purchase Now</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="books">
+            <div class="book-desc">
+                <h2>Surprise</h2>
+                <h3>A Storybook Quilt</h3>
+                <p class="book-passage">Rhymes delight the old and young<br>They slip off the reader’s tongue<br>Quilts soothe our active minds<br>Bring joy and time to unwind</p>
+                <p class="book-explanation">Discover a new world where animals from the quilt come alive...</p>
+                <p class="book-snippets">Turning the corner he was suprised to see.<br>A black and white skunk climbing a tree…</p>
+                <p class="book-snippets">A green garter snake sipping his tea.<br>And a red ant dipping his toe in the sea</p>
+                <p class="book-explanation">These are just some of the lines to be found in this delightful child’s picture book. The vibrant colors and textures jump from the pages and stimulate the senses of children and quilters alike. The book evolved from a quilt and poem I created for one of my grandsons.</p>
+            </div>
+
+            <div class="container">
+                <div class="book-images">
+                    <img class="book-cover-img" src="imgs/surprise-cover-small.jpg" alt="Book Cover">
+                    <div class="thumbnails">
+                        <img src="imgs/skunk-thumbnail.jpg" alt="Skunk page">
+                        <img src="imgs/hippo-thumbnail.jpg" alt="Hippo page">
+                        <img src="imgs/centipede-thumbnail.jpg" alt="Centipede page">
+                    </div>
+                </div>  
+                <div class="buttons">
+                    <p class="more-info-button">More Books</p>
+                    <p class="purchase-button">Purchase Now</p>
+                </div>
+            </div>
+        </section>
+    </main>
+    </div>
+
+    <footer>
+        <?php
+            include 'templates/footer.php';
+        ?>
+    </footer>
 
     <?php 
         include 'templates/scripts.php';
--- index.php
+++ index.php
@@ -54,35 +54,32 @@
         </section>
 
         <section class="books">
-        <h1>Featured Book</h1>
-        <div class="book-desc">
-            <h2>Surprise</h2>
-            <h3>A Storybook Quilt</h3>
-            <p class="book-passage">Rhymes delight the old and young<br>They slip off the reader’s tongue<br>Quilts soothe our active minds<br>Bring joy and time to unwind</p>
-            <p class="book-explanation">Discover a new world where animals from the quilt come alive...</p>
-            <p class="book-snippets">Turning the corner he was suprised to see.<br>A black and white skunk climbing a tree…</p>
-            <p class="book-snippets">A green garter snake sipping his tea.<br>And a red ant dipping his toe in the sea</p>
-            <p class="book-explanation">These are just some of the lines to be found in this delightful child’s picture book. The vibrant colors and textures jump from the pages and stimulate the senses of children and quilters alike. The book evolved from a quilt and poem I created for one of my grandsons.</p>
-        </div>
-
-        <div class="container">
-            <div class="book-images">
-                <img class="book-cover-img" src="imgs/surprise-cover-small.jpg" alt="Book Cover">
-                <div class="thumbnails">
-                    <img src="imgs/skunk-thumbnail.jpg" alt="Skunk page">
-                    <img src="imgs/hippo-thumbnail.jpg" alt="Hippo page">
-                    <img src="imgs/centipede-thumbnail.jpg" alt="Centipede page">
-                </div>
-            </div>  
-            <div class="buttons">
-                <p class="more-info-button">More Books</p>
-                <p class="purchase-button">Purchase Now</p>
+            <h1>Featured Book</h1>
+            <div class="book-desc">
+                <h2>Surprise</h2>
+                <h3>A Storybook Quilt</h3>
+                <p class="book-passage">Rhymes delight the old and young<br>They slip off the reader’s tongue<br>Quilts soothe our active minds<br>Bring joy and time to unwind</p>
+                <p class="book-explanation">Discover a new world where animals from the quilt come alive...</p>
+                <p class="book-snippets">Turning the corner he was suprised to see.<br>A black and white skunk climbing a tree…</p>
+                <p class="book-snippets">A green garter snake sipping his tea.<br>And a red ant dipping his toe in the sea</p>
+                <p class="book-explanation">These are just some of the lines to be found in this delightful child’s picture book. The vibrant colors and textures jump from the pages and stimulate the senses of children and quilters alike. The book evolved from a quilt and poem I created for one of my grandsons.</p>
             </div>
-        </div>
-        
-       
-    </section>
-   
+
+            <div class="container">
+                <div class="book-images">
+                    <img class="book-cover-img" src="imgs/surprise-cover-small.jpg" alt="Book Cover">
+                    <div class="thumbnails">
+                        <img src="imgs/skunk-thumbnail.jpg" alt="Skunk page">
+                        <img src="imgs/hippo-thumbnail.jpg" alt="Hippo page">
+                        <img src="imgs/centipede-thumbnail.jpg" alt="Centipede page">
+                    </div>
+                </div>  
+                <div class="buttons">
+                    <p class="more-info-button">More Books</p>
+                    <p class="purchase-button">Purchase Now</p>
+                </div>
+            </div>
+        </section>
     </main>
     
     <footer>
