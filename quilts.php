4a8c3ea9d49edc8e526494c3e9e177ce5df1d48c
maccuinn <april.l.hall@gmail.com>
2018-06-07 20:48:43 -0700

    put in breakpoints for quilts page

--- quilts.php
+++ quilts.php
@@ -12,13 +12,15 @@
 
     <div class="quilts-page">
         <main>
-            <div class="images">
+            <h1>Quilts</h1>
+            <div class="cards">
+            <div class="images" id="first">
                 <img src="imgs/marvel-quilt.png" alt="">
             </div>
             <div class="write-up">
                 <h2>Millicent Marvel &amp Bentley Bunny</h2>
                 <h3>A Storyboard Quilt</h3>
-                <p>Millicent Marvel left Bentley Bunny outside in the cold last night.<br>Her Gramma will make Bentley a quilt to warm up in but Millicent must choose a pattern.<br>Decisions, decisions.</p>
+                <p>My successful attempt at turning my Surprise quilt into a picture book inspired me to try again. When my fourth granddaughter was born I was so taken with her name, Millicent Marvel, that I decided to write a story and create another Storybook Quilt. Millicent lives on the Sunshine Coast of British Columbia in a hundred year old farmhouse. There are chickens scratching in the yard, a bear that slept there for a summer and bunnies in a pen. All wonderful fodder for a story. For this quilt, as well as raw edged appliqué and thread painting, I tried some new techniques. Each page was made as a discreet block. This made it much easier to handle on my sewing machine. An extra backing of a looped fabric added to the back allowed me to Velcro the pieces together in four different formats, 6 x 5 blocks, 5 x 6 blocks, 10 x 3 blocks and last but not least (when I displayed them in our local library), 30 x 1.  I also purchased a ScanNcut to cut out the words from Siser’s easy-weed strip-flock vinyl. First I formatted them on my computer and then sent them over to the ScanNcut. I learned the hard way that one must always reverse them before you cut. After that they were pressed on to the quilt blocks by a local t-shirt shop, just in time for the Boundary Bay Quilter Guild semi-annual quilt show.</p>
             </div>
             <div class="images">
                 <img src="imgs/surprise-quilt.png" alt="">
@@ -34,6 +36,7 @@
             </div>
             <div class="write-up">
                 <h2>William's Quilt</h2>
+                <p>Inspiration for this baby quilt came from the new parents. My son and his wife wanted race cars and I wanted an I-Spy quilt. The solution, two quilts folded into one. I created a fairly simple panel with zigzagging sashing in purple. After exhaustive searching I found a contrasting yellow Velcro. The sashing not only joined the I Spy panels but also became the race track. The dotted yellow line is formed from pieces of Velcro spaced down the middle. I created five different race cars, one for William, the new arrival, and one each for Mom, Dad, Grandpa and me. The base of the race cars were cut out of vinyl, the fabric was sewn on with raw edge appliqué and as a final step, I sewed the hook part of the Velcro to the race car backs. Then I created a pocket label to store the cars in when they weren’t on the track. To add more play value to the quilt, I used a pre-printed panel with city roads and of course another race track.</p>
             </div>
 
             <div class="images">
@@ -41,6 +44,7 @@
             </div>
             <div class="write-up">
                 <h2>The Skimmerhorns</h2>
+                <p>In the fall of 2016 I attended a Linear Landscapes class by Gloria Loughman from Australia which I greatly enjoyed. I would recommend her classes to anyone, she is an awesome teacher. I started this quilt with a photo of the looming Purcell mountains on the east side of the Creston Valley in British Columbia. This section of the range has been dubbed locally as the Skimmerhorns.</p>
             </div>
 
             <div class="images">
@@ -48,6 +52,7 @@
             </div>
             <div class="write-up">
                 <h2>Mt. Baker Night Owl</h2>
+                <p>While driving to a cabin near Mt. Baker Washington, my friends and I overshot our destination. Before we knew it we were twenty miles past it up the narrow windy mountain road near the ski hill. Heading back down in the pitch dark, an owl swooped down into our headlights and was gone. When I got home, I searched the net for a picture to match my memory. The bird’s feathers were pieced with a fabric that featured grasses and moths. I even included some moths in the upper back of the bird. I thread painted the highlights of the moon on the owl’s wings. The background was strip pieced to give the illusion of forest.</p>
             </div>
 
             <div class="images">
@@ -55,6 +60,8 @@
             </div>
             <div class="write-up">
                 <h2>Retro Roadtrip</h2>
+                <p>When my husband and I attended the Vintage Car Club of Canada’s May tour at Qualicum Beach on Vancouver Island, one of the runs took us to Courtenay where we enjoyed a boxed lunch at the city park. At the entrance to the park, two totem poles stood guard. After carefully lining up our 1956 Chevy Belair nine passenger station wagon between the totems I snapped a photograph to add to my quilt ideas folder. When it came time to work on the quilt, I used a totem pole printed fabric. The size of the poles determined the final size of the quilt. Keeping true to the proportions in the photo I created a new background for the quilt. Then I enlarged the car in my photo to match, printed it on white cotton, added raw edged appliqué and thread painted it. I sewed the finished car onto my background. While the totem poles are from a fabric print, they are also extensively fabric painted.</p>
+            </div>
             </div>
         </main>
     </div>
