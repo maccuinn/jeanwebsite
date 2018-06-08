a2f461790129d68a5b33e6229c0ec4b3020b16d7
maccuinn <april.l.hall@gmail.com>
2018-06-07 20:49:57 -0700

    took out navigation from footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,6 @@
 <div class="footer-container">
-    <nav>
-        <ul class="col-one">
-            <li><a href="#0">Home</a></li>
-            <li><a href="#0">Books</a></li> 
-        </ul>
-        <ul class="col-two"> 
-            <li><a href="quilts.html">Quilts</a></li>
-            <li><a href="#0">Contact</a></li>
-        </ul>
-    </nav>
     <div class="copyright">
         <p>&copy 2018 April Hall</p>
         <p>All images &copy Jean Husband</p>
     </div>
-    
 </div>
