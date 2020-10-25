4b57cad5d7c5a45e9fa157d65f340ed85b2a9408
Jamie Pate <jpate@moscore.com>
2020-10-24 19:05:54 -0700

    Add google analytics to the head

--- templates/head.php
+++ templates/head.php
@@ -11,4 +11,13 @@ if(!isset($pgName)){
     <link href="../styles/styles.css" type="text/css" rel="stylesheet">
     <link rel="import" href="bower_components/dsc-polymer-carousel/dsc-polymer-carousel.html">
     <title>Ingenius Creations - <?php echo $pgName; ?></title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WNWBQ9MLW3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WNWBQ9MLW3');
+    </script>
 </head>
