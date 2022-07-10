bc97adafa4f79d112afe5691c980b5efcaa1137a
Jamie Pate <jpate@moscore.com>
2022-07-10 11:23:35 -0700

    Set up MMSS as the featured book.

--- templates/featured-book.php
+++ templates/featured-book.php
@@ -1,24 +1,24 @@
-
+<?php $buy= "https://www.amazon.ca/Millicent-Marvel-Bentley-Bunny-Salish/dp/0995200750/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1656882831&sr=8-2" ?>
 <div class="book-desc-container">
-    <iframe class="book-image" type="text/html" width="414" height="724" frameborder="0" allowfullscreen style="max-width:100%" src="https://read.amazon.ca/kp/card?asin=B08L5SR3T4&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_RD1IFbHYJWH1R" ></iframe>
+    <iframe class="book-image" type="text/html" width="414" height="724" frameborder="0" allowfullscreen style="max-width:100%"
+        src="https://read.amazon.ca/kp/card?asin=0995200750&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_RD1IFbHYJWH1R" ></iframe>
     <div class="book-desc">
-        <h2>Tangled Tongue</h2>
-        <h3>A F.U.S.E Adventure <sub>Book One</sub></h3>
+        <h2>Millicent Marvel & Bentley Bunny By the Salish Sea</h2>
         <p class="book-explanation">
-            A wicked stutter is peanuts compared to rescuing your dad.
+            Millicent Marvel sets out with Bentley Bunny on a small adventure while Mama snores on the picnic blanket. Millicent explores the forest and encounters everything from slugs to owls. When she wanders down to the Salish Sea, an orca greets her. The sun is setting and Mama is frantic. Where is Millicent Marvel?
         </p>
-
+        You can contact the author for a book or find it on Amazon in ebook or paperback book form:
         <a class="buy-button" style="align-self: flex-end"
-                href="https://www.amazon.com/Tangled-Tongue-F-U-S-Adventure-ebook/dp/B08L5SR3T4/ref=sr_1_1?dchild=1&qid=1602869977&refinements=p_27%3AJean+Husband&s=books&sr=1-1">
+                href="<?= $buy ?>">
             Buy now
         </a>
-        <p class="book-explanation">
-        Duff Fraser can’t leave his stutter behind even on the soccer field. Ambushed by a bully from the rival team, he limps home. Duff and his dad team up to create the F.U.S.E., a device that alters brain pathways and lets Duff speak normally. Then hoodlums snatch his dad off the street. Will Duff, armed with newfound confidence, robots, drones, and best of all, good friends, be able to find him?
-        </p>
 
-        <p class="book-explanation">
-        Tangled Tongue is a coming of age quest novel comparable to The Truth as Told by Mason Buttle and Fish in a Tree. This middle-grade adventure features self-reliance, friendship, and technology. A perfect read for back to school — no matter what that looks like.
-        </p>
+        <dsc-polymer-carousel interval="5" style="--carousel-width:210px" width="210px">
+            <section background="imgs/mmss/MMSS cover.png"><a href="<?= $buy ?>"></a></section>
+            <section background="imgs/mmss/MMSS mosquito.png"><a href="<?= $buy ?>"></a></section>
+            <section background="imgs/mmss/MMSS orca.png"><a href="<?= $buy ?>"></a></section>
+            <section background="imgs/mmss/MMSS slug.png"><a href="<?= $buy ?>"></a></section>
+        </dsc-polymer-carousel>
 
     </div>
 </div>
